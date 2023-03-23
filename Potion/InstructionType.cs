ad00bb6c029b51694af4d16a02da8367a92d9093
jvyden <jvyden@jvyden.xyz>
2023-03-22 20:35:34 -0400

    Document

--- Potion/InstructionType.cs
+++ Potion/InstructionType.cs
@@ -6,14 +6,32 @@ public enum InstructionType : byte
     
     Add = 0xA1,
     Sub = 0xA2,
+    /// <summary>
+    /// Sets a value in the register
+    /// </summary>
     Set = 0xB0,
 
+    /// <summary>
+    /// Print a character to the console
+    /// </summary>
     Print = 0xD1,
+    /// <summary>
+    /// Read a character from the console
+    /// </summary>
     Read = 0xD2,
     
     Jmp = 0xDA,
+    /// <summary>
+    /// Jumps if A and B are equal
+    /// </summary>
     JmpE = 0xDB,
+    /// <summary>
+    /// Jumps if A and B are not equal
+    /// </summary>
     JmpNe = 0xDC,
     
+    /// <summary>
+    /// Stops execution
+    /// </summary>
     Hlt = 0xDF,
 }
